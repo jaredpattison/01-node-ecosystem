280f44b0ee969572dc3cc3b841b757d2664619aa
jared <jaredpattison@users.noreply.github.com>
2018-12-17 23:06:02 -0800

    test

--- __tests__/arithmetic.test.js
+++ __tests__/arithmetic.test.js
@@ -2,3 +2,30 @@
 
 const arithmetic = require('../lib/arithmetic.js');
 
+describe('arithmetic module', () => {
+
+  it ('requires numeric input', () => {
+    let expected = null;
+    let sum = arithmetic.add('word', 3);
+    expect(sum).toEqual(expected);
+  });
+
+  it('can add 2 numbers', () => {
+    let expected = 2;
+    let sum = arithmetic.add(1, 1);
+    expect(sum).toEqual(expected);
+  });
+
+  it ('requires numeric input', () => {
+    let expected = null;
+    let sum = arithmetic.subtract('word', 3);
+    expect(sum).toEqual(expected);
+  });
+
+  it('can add 2 numbers', () => {
+    let expected = 0;
+    let sum = arithmetic.subtract(1, 1);
+    expect(sum).toEqual(expected);
+  });
+
+});
