c3a5f7dad0c6ce0ae40db31956cc00872f658dd9
jared <jaredpattison@users.noreply.github.com>
2018-12-29 17:29:28 -0800

    more tests, lab complete

--- __tests__/arithmetic.test.js
+++ __tests__/arithmetic.test.js
@@ -1,6 +1,11 @@
 'use strict';
 
 const arithmetic = require('../lib/arithmetic.js');
+let faker = require('faker');
+
+let firstRandom = faker.random.number();
+
+let secondRandom = faker.random.number();
 
 describe('arithmetic module', () => {
 
@@ -11,21 +16,44 @@ describe('arithmetic module', () => {
   });
 
   it('can add 2 numbers', () => {
-    let expected = 2;
-    let sum = arithmetic.add(1, 1);
+    let expected = firstRandom + secondRandom;
+    let sum = arithmetic.add(firstRandom, secondRandom);
     expect(sum).toEqual(expected);
   });
 
   it ('requires numeric input', () => {
     let expected = null;
-    let sum = arithmetic.subtract('word', 3);
-    expect(sum).toEqual(expected);
+    let subtract = arithmetic.subtract('word', 3);
+    expect(subtract).toEqual(expected);
   });
 
   it('can add 2 numbers', () => {
-    let expected = 0;
-    let sum = arithmetic.subtract(1, 1);
-    expect(sum).toEqual(expected);
+    let expected = firstRandom - secondRandom;
+    let subtract = arithmetic.subtract(firstRandom, secondRandom);
+    expect(subtract).toEqual(expected);
   });
 
+  it ('requires numeric input', () => {
+    let expected = null;
+    let multiply = arithmetic.multiply('word', 3);
+    expect(multiply).toEqual(expected);
+  });
+
+  it('can multiply 2 numbers', () => {
+    let expected = firstRandom * secondRandom;
+    let multiply = arithmetic.multiply(firstRandom, secondRandom);
+    expect(multiply).toEqual(expected);
+  });
+
+  it ('requires numeric input', () => {
+    let expected = null;
+    let divide = arithmetic.divide('word', 3);
+    expect(divide).toEqual(expected);
+  });
+
+  it ('can divide 2 numbers', () => {
+    let expected = firstRandom / secondRandom;
+    let divide = arithmetic.divide(firstRandom, secondRandom);
+    expect(divide).toEqual(expected);
+  });
 });
